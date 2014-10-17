89af9939226d43623302a69cdf47ebc40f71ae07
Jake Crane <jakecrane91@gmail.com>
2014-10-17 10:54:44 -0500

    Changed field of view from 120 to 45 moved camera to better center plotted points reduced number of points Added x and y axis Added new random fucntion increased point size from 1 to 2

--- Clouds2.cpp
+++ Clouds2.cpp
@@ -18,11 +18,58 @@ using namespace glm;
 #include <shader.hpp>
 
 struct vertex {
-	float x, y, z;
-	float r, g, b;
+	GLfloat x, y, z;
+	GLfloat r, g, b;
 };
 
+/*
+long niminus1 = 8l;
+long myrand(long min, long max) {
+	long range = max - min;
+	niminus1 = (niminus1 * 5l + 12345l);
+	return niminus1 % max;
+}
+
+long jsw_lcg(long max) {
+	niminus1 = ( 2l * niminus1 + 3l ) % 10l;
+	return niminus1 % max;
+}
+ */
+
+static int seed = 1;
+
+#define M 2147483647
+#define A 16807
+#define Q ( M / A )
+#define R ( M % A )
+
+int jsw_rand(int min, int max) {
+	seed = A * ( seed % Q ) - R * ( seed / Q );
+	if ( seed <= 0 )
+		seed += M;
+	//return seed % (max + 1);
+	return min + ( seed % ( (max + 1) - min ) );
+}
+
 int main() {
+
+	/*printf("jsw_lcg():\n");
+	for (int i = 0; i < 20; i++) {
+		printf("%ld\n", jsw_lcg(15));
+	}
+	printf("\n");*/
+
+	printf("jsw_rand():\n");
+	for (int i = 0; i < 20; i++) {
+		printf("%d\n", jsw_rand(4, RAND_MAX));
+	}
+	printf("\n");
+
+	/*printf("myrand():\n");
+	for (int i = 0; i < 20; i++) {
+		printf("%ld\n", myrand(0, 10));
+	}*/
+
 	// Initialise GLFW
 	if(!glfwInit()) {
 		fprintf( stderr, "Failed to initialize GLFW\n" );
@@ -67,14 +114,14 @@ int main() {
 	GLuint MatrixID = glGetUniformLocation(programID, "MVP");
 
 	// Projection matrix : 45° Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
-	glm::mat4 Projection = glm::perspective(120.0f, 4.0f / 3.0f, 0.0f, 100.0f);
+	glm::mat4 Projection = glm::perspective(45.0f, 4.0f / 3.0f, 0.0f, 100.0f);
 	// Or, for an ortho camera :
 	//glm::mat4 Projection = glm::ortho(-1.0f, 50.0f, -1.0f,50.0f,0.0f,100.0f); // In world coordinates
 
 	// Camera matrix
 	glm::mat4 View       = glm::lookAt(
-			glm::vec3(22500, 16700, 10000), // Camera is at (4,3,3), in World Space
-			glm::vec3(22500, 16700, 0), // and looks at the origin
+			glm::vec3(17500, 16700, 45000), // Camera is at (4,3,3), in World Space
+			glm::vec3(17500, 16700, 0), // and looks at the origin
 			glm::vec3(0, 1, 0)  // Head is up (set to 0,-1,0 to look upside-down)
 	);
 	// Model matrix : an identity matrix (model will be at the origin)
@@ -82,27 +129,57 @@ int main() {
 	// Our ModelViewProjection : multiplication of our 3 matrices
 	glm::mat4 MVP = Projection * View * Model; // Remember, matrix multiplication is the other way around
 
-	const int vertexLength = 5000;
+	const int vertexLength = 7000;
 
 	vertex g_vertex_buffer_data[vertexLength];
 
 	srand(time(NULL)); //initialize random seed
 
-	for (int i = 0; i < vertexLength; i++) {
+	/*for (int i = 0; i < vertexLength - 3; i++) {
 		g_vertex_buffer_data[i].x = rand();
 		g_vertex_buffer_data[i].y = rand();
 		g_vertex_buffer_data[i].z = 0.0f;
 		g_vertex_buffer_data[i].r = (rand() % 101) / 100.0f;
 		g_vertex_buffer_data[i].g = (rand() % 101) / 100.0f;
 		g_vertex_buffer_data[i].b = (rand() % 101) / 100.0f;
-	}
+	}*/
+
+	for (int i = 0; i < vertexLength - 3; i++) {
+			g_vertex_buffer_data[i].x = jsw_rand(0, RAND_MAX);
+			g_vertex_buffer_data[i].y = jsw_rand(0, RAND_MAX);
+			g_vertex_buffer_data[i].z = 0.0f;
+			g_vertex_buffer_data[i].r = jsw_rand(0, 101) / 100.0f;
+			g_vertex_buffer_data[i].g = jsw_rand(0, 101) / 100.0f;
+			g_vertex_buffer_data[i].b = jsw_rand(0, 101) / 100.0f;
+		}
+
+	g_vertex_buffer_data[vertexLength - 3].x = 0;
+	g_vertex_buffer_data[vertexLength - 3].y = RAND_MAX;
+	g_vertex_buffer_data[vertexLength - 3].z = 0.0f;
+	g_vertex_buffer_data[vertexLength - 3].r = 1;
+	g_vertex_buffer_data[vertexLength - 3].g = 1;
+	g_vertex_buffer_data[vertexLength - 3].b = 1;
+
+	g_vertex_buffer_data[vertexLength - 2].x = 0;
+	g_vertex_buffer_data[vertexLength - 2].y = 0;
+	g_vertex_buffer_data[vertexLength - 2].z = 0.0f;
+	g_vertex_buffer_data[vertexLength - 2].r = 1;
+	g_vertex_buffer_data[vertexLength - 2].g = 1;
+	g_vertex_buffer_data[vertexLength - 2].b = 1;
+
+	g_vertex_buffer_data[vertexLength - 1].x = RAND_MAX;
+	g_vertex_buffer_data[vertexLength - 1].y = 0;
+	g_vertex_buffer_data[vertexLength - 1].z = 0.0f;
+	g_vertex_buffer_data[vertexLength - 1].r = 1;
+	g_vertex_buffer_data[vertexLength - 1].g = 1;
+	g_vertex_buffer_data[vertexLength - 1].b = 1;
 
 	GLuint vertexbuffer;
 	glGenBuffers(1, &vertexbuffer);
 	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
 
-	glPointSize(1.0);
+	glPointSize(2.0);
 
 	do {
 
@@ -116,14 +193,13 @@ int main() {
 		// in the "MVP" uniform
 		glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
 		glEnableVertexAttribArray(0);
-		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
 		glVertexAttribPointer(
 				0,                  // attribute. No particular reason for 0, but must match the layout in the shader.
 				3,                  // size
 				GL_FLOAT,           // type
 				GL_FALSE,           // normalized?
-				sizeof(vertex),                // stride
-				(void*) offsetof(vertex, x)          // array buffer offset
+				sizeof(vertex),     // stride
+				(void*) offsetof(vertex, x) // array buffer offset
 		);
 
 		glEnableVertexAttribArray(1);
@@ -132,13 +208,13 @@ int main() {
 				3,                  // size
 				GL_FLOAT,           // type
 				GL_FALSE,           // normalized?
-				sizeof(vertex),                  // stride
-				(void*) offsetof(vertex, r)             // array buffer offset
+				sizeof(vertex),     // stride
+				(void*) offsetof(vertex, r) // array buffer offset
 		);
 
-
 		// Draw the triangle !
-		glDrawArrays(GL_POINTS, 0, vertexLength); // 3 indices starting at 0 -> 1 triangle
+		glDrawArrays(GL_LINE_STRIP, vertexLength - 3, 3);
+		glDrawArrays(GL_POINTS, 0, vertexLength - 3); // 3 indices starting at 0 -> 1 triangle
 		glDisableVertexAttribArray(0);
 		glDisableVertexAttribArray(1);
 
